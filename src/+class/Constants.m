76749ba726864037830c874c97c23c385a3f9e44
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2026-03-06 23:29:51 -0500

    Sync uipanel & dockcontainer positions

--- src/+class/Constants.m
+++ src/+class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'monitorSPED'
         appRelease    = 'R2024a'
-        appVersion    = 'beta_3.01.9'
+        appVersion    = 'beta_3.01.10'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 950, 660]
