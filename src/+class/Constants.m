6dd6f4a1fd50c4e4cb140cdd6529a787fe4721fd
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2026-03-17 23:38:28 -0400

    Update GeneralSettings.json

--- src/+class/Constants.m
+++ src/+class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'monitorSPED'
         appRelease    = 'R2024a'
-        appVersion    = 'beta_3.03.0'
+        appVersion    = 'beta_3.03.1'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 950, 660]
